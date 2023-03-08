d73e340f588744e210ab70509bfc4b82d5eb8e55
Aygul <Your>
2023-03-08 13:58:43 +0300

    Example008_IntroMethod

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 7, 2, 12, 31, 4, 18, 15, 16, 17, 2, 7, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
