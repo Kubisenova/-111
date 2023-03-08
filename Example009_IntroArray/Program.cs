613f31b1c25553943c3cc6c298fe62686fd759b6
Aygul <Your>
2023-03-09 02:16:57 +0300

    Сохранение

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -5,7 +5,7 @@
     if (arg3 > result) result = arg3;
     return result;
 }
-//        
+   
 
 // // здесь показано каким образом можно обратиться к массиву и записать его значение 
 // array[0] = 12;
@@ -20,5 +20,6 @@ int result = Max(
     Max(array[6], array[7], array[8])
 );
 Console.WriteLine(result);
-       0   1   2   3   4   5   6   7   8
+                 0    1   2   3   4   5   6   7   8
 int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
+
